2ba02e6bdaf37bd71b923ede7e24ebdbd774e820
sugyan <sugi1982@gmail.com>
2023-09-13 22:39:08 +0900

    Add benchmarks

---
+++ pentomino-solver/benches/benchmark.rs
@@ -0,0 +1,14 @@
+#![feature(test)]
+extern crate test;
+use pentomino_solver::solvers::SimpleSolver;
+use pentomino_solver::Solver;
+
+#[bench]
+fn bench_8x8_2x2(b: &mut test::Bencher) {
+    let solver = SimpleSolver::new(8, 8);
+    let start = [27, 28, 35, 36].iter().map(|&p| 1 << p).sum::<u64>();
+    b.iter(|| {
+        let solutions = solver.solve(start);
+        assert_eq!(solutions.len(), 520);
+    });
+}
