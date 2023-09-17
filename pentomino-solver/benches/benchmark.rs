0a2b07a1717a4183f93e0daaec01d650be850462
sugyan <sugi1982@gmail.com>
2023-09-17 23:29:12 +0900

    Update benchmarks

--- pentomino-solver/benches/benchmark.rs
+++ pentomino-solver/benches/benchmark.rs
@@ -1,13 +1,54 @@
 #![feature(test)]
 extern crate test;
-use pentomino_solver::solvers::{SimpleSolver, Solver};
+use pentomino_solver::solvers::{DefaultSolver, OptimizedSolver, SimpleSolver};
+use pentomino_solver::Solver;
 
 #[bench]
-fn bench_8x8_2x2(b: &mut test::Bencher) {
+fn bench_8x8_2x2_simple(b: &mut test::Bencher) {
     let solver = SimpleSolver::new(8, 8);
     let initial = [27, 28, 35, 36].iter().map(|&p| 1 << p).sum::<u64>().into();
     b.iter(|| {
         let solutions = solver.solve(initial, false);
         assert_eq!(solutions.len(), 520);
+    });
+}
+
+#[bench]
+fn bench_8x8_2x2_default(b: &mut test::Bencher) {
+    let solver = DefaultSolver::new(8, 8);
+    let initial = [27, 28, 35, 36].iter().map(|&p| 1 << p).sum::<u64>().into();
+    b.iter(|| {
+        let solutions = solver.solve(initial, false);
+        assert_eq!(solutions.len(), 520);
+    });
+}
+
+#[bench]
+fn bench_8x8_2x2_optimized(b: &mut test::Bencher) {
+    let solver = OptimizedSolver::new(8, 8);
+    let initial = [27, 28, 35, 36].iter().map(|&p| 1 << p).sum::<u64>().into();
+    b.iter(|| {
+        let solutions = solver.solve(initial, false);
+        assert_eq!(solutions.len(), 520);
+    });
+}
+
+#[bench]
+fn bench_8x8_2x2_default_unique(b: &mut test::Bencher) {
+    let solver = DefaultSolver::new(8, 8);
+    let initial = [27, 28, 35, 36].iter().map(|&p| 1 << p).sum::<u64>().into();
+    b.iter(|| {
+        let solutions = solver.solve(initial, true);
+        assert_eq!(solutions.len(), 65);
+    });
+}
+
+#[bench]
+fn bench_8x8_2x2_optimized_unique(b: &mut test::Bencher) {
+    let solver = OptimizedSolver::new(8, 8);
+    let initial = [27, 28, 35, 36].iter().map(|&p| 1 << p).sum::<u64>().into();
+    b.iter(|| {
+        let solutions = solver.solve(initial, true);
+        assert_eq!(solutions.len(), 65);
     });
 }
